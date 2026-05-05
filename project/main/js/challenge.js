5b09e50064c2afce2828f5797752cbfeba316d09
martin-rammerstorfer <m.rammerstorfer@students.htl-leonding.ac.at>
2026-05-05 12:11:46 +0200

    Add challenge data

--- project/main/js/challenge.js
+++ project/main/js/challenge.js
@@ -1,59 +1,53 @@
 let challenges = [
     {
-        title: "Challenge 1",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        title: "Emptiness",
+        description: "nothing to see here.",
+        format: "The flag is in the format KDCTF{...}",
         points: 100,
         difficulty: "easy",
-        category: "Image",
-        file: ""
+        category: "file",
+        file: "challenge_files/emptiness/emptiness.txt",
+        flag: "KDCTF{wh4t_4_n1c3_qr_c0d3}"
     },
     {
-        title: "Challenge 2",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        points: 200,
+        title: "Jingle Bells",
+        description: "A classic! The lyrics are about bells and also some guy riding in a carriage",
+        format: "The flag is in the format FLAG{...}",
+        points: 300,
         difficulty: "medium",
-        category: "Image",
-        file: ""
+        category: "file",
+        file: "challenge_files/jingle_bells/jingle_bells",
+        flag: "FLAG{j1ngl3_b3lls_b4tm4n_sm3lls}"
     },
     {
-        title: "Challenge 3",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        title: "Elliot",
+        description: "02:48 -!- alderson [~alderson@unaffiliated/alderson] has joined #fsociety 02:48 -!- Topic for #fsociety: Preparing for Stage 2 | Evil Corp must burn! 02:48 -!- Topic set by mrrobot [] [Fri Nov 4 14:47:04 2016] 02:52 < alderson> What is Stage 2??? 02:53 <+mrrobot> dont you remember? 02:53 <+mrrobot> you created the plans for it 02:53 < alderson> Are you hunter2 crazy? 02:54 <+mrrobot> no need for profanity. anyways you said if you ever forgot about Stage 2 that i should give you this file 02:55 <+mrrobot> i have no idea how this picture of yourself is going to help you remember, but here it is anyways:",
+        format: "The flag is in the format KDCTF{...}",
+        points: 300,
+        difficulty: "medium",
+        category: "image",
+        file: "challenge_files/elliot/Elliot.png",
+        flag: "KDCTF{ev1l_c0rp_1s_n0t_y0ur_fr13nd}"
+    },
+    {
+        title: "Blackout",
+        description: "Copied files, blackout, file broken, help pls. T_T",
+        format: "The flag is in the format KDCTF{...}",
+        points: 300,
+        difficulty: "medium",
+        category: "file",
+        file: "challenge_files/blackout/secret",
+        flag: "KDCTF{s4mpl3_fl4g}"
+    },
+    {
+        title: "Ievian Polkka",
+        description: "Hra-tsa-tsa, ia ripi-dapi dilla barits tad dillan deh lando. Aba rippadta parip parii ba ribi, ribi, ribiriz den teahlando, La barillaz dillan deiallou ara va reve reve revydyv dyvjavuo Bariz dah l'llavz dei lando dabaoke dagae gadae due due dei ia do Hra-tsa-tsa, ia ripi-dapi dilla barits tad dillan deh lando. Aba rippadta parip parii ba ribi, ribi, ribiriz den teahlando, La barillaz dillan deiallou ara va reve reve revydyv dyvjavuo Bariz dah l'llavz dei lando dabaoke dagae gadae due due dei ia do.",
+        format: "The flag is in the format KDCTF{...}",
         points: 500,
         difficulty: "hard",
-        category: "Image",
-        file: ""
-    },
-    {
-        title: "Challenge 4",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        points: 250,
-        difficulty: "medium",
-        category: "Image",
-        file: ""
-    },
-    {
-        title: "Challenge 5",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        points: 300,
-        difficulty: "medium",   
-        category: "Image",
-        file: ""
-    },
-    {
-        title: "Challenge 6",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        points: 100,
-        difficulty: "easy",
-        category: "Image",
-        file: ""
-    },
-    {
-        title: "Challenge 7",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        points: 150,
-        difficulty: "easy",
-        category: "Image",
-        file: ""
+        category: "video",
+        file: "challenge_files/ievian_polkka/Ievian_Polkka.mp4",
+        flag: "KDCTF{m1ku_l0v3s_h3r_l33k}"
     }
 ];
 
